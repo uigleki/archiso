5a7f3485f28e5167b1e139454b989b15cba00ad4
ray <rraayy246@gmail.com>
2023-01-18 03:48:01 +0800

    rm .bash

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -3,6 +3,7 @@ systemctl enable firewalld bluetooth NetworkManager sddm
 
 sed -i '/# %wheel .* NOPASSWD/s/# //' /etc/sudoers
 
+find /etc/skel -name '.bash*' | xargs rm -rf
 useradd -m -G wheel -s /bin/zsh arch
 passwd -d arch
 passwd -u arch
