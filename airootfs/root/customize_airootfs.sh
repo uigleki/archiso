4f0dfece7a53e6db486037eff20a37b55dca8a06
ray <rraayy246@gmail.com>
2023-01-17 23:43:33 +0800

    login

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,2 +1,8 @@
 systemctl set-default graphical.target
-systemctl enable sddm
+systemctl enable sddm NetworkManager
+
+sed -i '/# %wheel .* NOPASSWD/s/# //' /etc/sudoers
+
+useradd -m -G wheel -s /bin/zsh arch
+passwd -d arch
+passwd -u arch
