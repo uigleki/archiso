061f0bf2c6f544353971da7d7e0335ba1f65c80d
ray <rraayy246@gmail.com>
2023-01-18 02:02:14 +0800

    a

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,8 +1,17 @@
 systemctl set-default graphical.target
-systemctl enable sddm NetworkManager
+systemctl enable firewalld bluetooth NetworkManager sddm
 
 sed -i '/# %wheel .* NOPASSWD/s/# //' /etc/sudoers
 
 useradd -m -G wheel -s /bin/zsh arch
 passwd -d arch
 passwd -u arch
+
+fish env.fish
+sudo -u arch fish env.fish
+
+cat << EOF | sudo tee $HOME/.Xmodmap
+! 将 CapsLock 作为额外的 Home 键
+remove Lock = Caps_Lock
+keysym Caps_Lock = Home
+EOF
