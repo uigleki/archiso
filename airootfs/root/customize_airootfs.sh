8edf7d0ecb092d52a847d830650f5263fd69c661
ray <rraayy246@gmail.com>
2023-01-18 03:50:24 +0800

    a

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -10,9 +10,3 @@ passwd -u arch
 
 fish ~/env.fish
 sudo -u arch fish ~/env.fish
-
-cat << EOF | sudo tee $HOME/.Xmodmap
-! 将 CapsLock 作为额外的 Home 键
-remove Lock = Caps_Lock
-keysym Caps_Lock = Home
-EOF
