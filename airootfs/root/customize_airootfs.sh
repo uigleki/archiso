946b583746fd7c5778b42840b1132329ee11530e
ray <rraayy246@gmail.com>
2023-01-18 14:50:05 +0800

    a

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,3 +1,8 @@
+sed -i '/#\(en_US\|zh_CN\).UTF-8/s/#//' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
 systemctl set-default graphical.target
 systemctl enable firewalld bluetooth NetworkManager sddm
 
@@ -5,6 +10,7 @@ sed -i '/# %wheel .* NOPASSWD/s/# //' /etc/sudoers
 
 find /etc/skel -name '.bash*' | xargs rm -rf
 cp -aT /etc/skel/ /root/
+
 useradd -m -G wheel -s /bin/zsh arch
 passwd -d arch
 passwd -u arch
