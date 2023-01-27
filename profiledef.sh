c0f5fc77b873fbe54fe0a1b6cbabab42b666563a
ray <rraayy246@gmail.com>
2023-01-27 14:51:36 +0800

    zstd

--- profiledef.sh
+++ profiledef.sh
@@ -13,8 +13,8 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=(-zlz4hc -E ztailpacking)
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=(-comp zstd -Xcompression-level 3 -b 1M)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )
